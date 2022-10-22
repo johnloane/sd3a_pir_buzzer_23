e2e3bde71c0a95b4f326174dbf943cacb0bb3340
johnloane <john.loane@dkit.ie>
2022-10-22 19:57:02 +0100

    Refactoring the code to use PubNub
    This will allow us to access the Pi and Sensors online

--- sensors.py
+++ sensors.py
@@ -1,5 +1,50 @@
 import RPi.GPIO as GPIO
-import time
+import time, threading
+
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory, PNOperationType
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = 'mySubscribeKey'
+pnconfig.publish_key = 'myPublishKey'
+pnconfig.user_id = "my_custom_user_id"
+pubnub = PubNub(pnconfig)
+
+my_channel = 'johns-sd3a-pi-channel'
+sensors_list=["buzzer"]
+data={}
+
+def my_publish_callback(envelope, status):
+    # Check whether request successfully completed or not
+    if not status.is_error():
+        pass  # Message successfully published to specified channel.
+    else:
+        pass  # Handle message publish error. Check 'category' property to find out possible issue
+        # because of which request did fail.
+        # Request can be resent using: [status retry];
+class MySubscribeCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass  # This event happens when radio / connectivity is lost
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            # Connect event. You can do stuff like publish, and know you'll get it.
+            # Or just use the connected event to confirm you are subscribed for
+            # UI / internal notifications, etc
+            pubnub.publish().channel(my_channel).message('Hello world!').pn_async(my_publish_callback)
+        elif status.category == PNStatusCategory.PNReconnectedCategory:
+            pass
+            # Happens as part of our regular operation. This event happens when
+            # radio / connectivity is lost, then regained.
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+            # Handle message decryption error. Probably client configured to
+            # encrypt messages and on live data feed it received plain text.
+    def message(self, pubnub, message):
+        # Handle new message stored in message.message
+        print(message.message)
 
 PIR_pin = 23
 Buzzer_pin = 24
@@ -27,6 +72,8 @@ def motion_detection():
             beep(4)
         time.sleep(1)
 
-
-time.sleep(1)
-motion_detection()
+if __name__ == '__main__':
+    sensors_thread = threading.Thread(target=motion_detection)
+    sensors_thread.start()
+    pubnub.add_listener(MySubscribeCallback())
+    pubnub.subscribe().channels(my_channel).execute()
